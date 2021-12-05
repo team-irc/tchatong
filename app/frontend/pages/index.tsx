d3ab133d4771e84bd5b27f30721341a260cac6da
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-12-05 17:07:32 +0900

    Add MUI/icons

--- app/frontend/pages/index.tsx
+++ app/frontend/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import { Button } from "@material-ui/core";
+import { Delete } from "@mui/icons-material";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.Home}>
       <Button variant="contained" color="primary">hello world</Button>
+      <Delete />
     </div>
   )
 }
