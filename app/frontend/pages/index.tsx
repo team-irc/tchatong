94a298c2004a97734ebc35d56a6fcee9b477ca4b
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-12-07 10:19:30 +0900

    Implement Mainpage Logo, AutoComplete Search Bar

--- app/frontend/pages/index.tsx
+++ app/frontend/pages/index.tsx
@@ -1,15 +1,39 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import { Button } from "@material-ui/core";
-import { Delete } from "@mui/icons-material";
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import { Box, TextField } from "@material-ui/core";
+import Autocomplete from '@mui/material/Autocomplete';
+import type { streamer } from './api/streamers';
+import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+const Home = ({ data }: InferGetServerSidePropsType<GetServerSideProps>) => {
   return (
-    <div className={styles.Home}>
-      <Button variant="contained" color="primary">hello world</Button>
-      <Delete />
-    </div>
-  )
+    <>
+      <main className={styles.Home}>
+        <Box className={styles.MainContent}>
+          <h1 className={styles.MainLogo}>트채통</h1>
+        </Box>
+        <Box className={styles.MainContent}>
+          <Autocomplete
+            disablePortal
+            options={data}
+            getOptionLabel={(data: streamer) => data.streamerName}
+            renderInput={(params) => {
+              return (<TextField label={"스트리머 닉네임을 입력해주세요."}
+                                 {...params}
+                                 className={styles.SearchText}
+                                 variant={"filled"} />)
+            }} />
+        </Box>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch("http://localhost/api/streamers", {method: "GET"});
+  const data: streamer[] = await res.json();
+  if (!data) return {notFound: true}
+  return {props: {data: data}};
 }
 
 export default Home
