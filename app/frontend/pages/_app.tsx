00c0d2ad644335529a729af66c2e29059e81e5e6
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-12-07 10:14:02 +0900

    Add app meta data

--- app/frontend/pages/_app.tsx
+++ app/frontend/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>트채통 | 트위치 채팅 통계 시스템</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
