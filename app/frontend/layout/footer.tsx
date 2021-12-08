d66b08d07299d4a98ff0bde71a817704346ff729
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-12-08 20:49:23 +0900

    Add search feature

--- app/frontend/layout/footer.tsx
+++ app/frontend/layout/footer.tsx
@@ -4,25 +4,28 @@ import { Button } from "@material-ui/core";
 import SupportAgentIcon from "@mui/icons-material/SupportAgent";
 import { FC } from "react";
 
-const Footer: FC = ({children}): JSX.Element => {
+const Footer: FC = ({ children }): JSX.Element => {
   return (
     <>
       {children}
       <footer className={styles.Footer}>
         <Link href="/help">
-          <Button variant="text"
-                  startIcon={<SupportAgentIcon />}
-                  size="large"
-                  className={styles.FooterButtonToServiceCenter}>
+          <Button
+            variant="text"
+            startIcon={<SupportAgentIcon />}
+            size="large"
+            className={styles.FooterButtonToServiceCenter}
+          >
             고객센터
           </Button>
         </Link>
         <div className={styles.FooterCopyRights}>
-          &copy; 2021 <a href="https://github.com/team-irc">team-irc</a> all rights reserved.
+          &copy; 2021 <a href="https://github.com/team-irc">team-irc</a> all
+          rights reserved.
         </div>
       </footer>
     </>
   );
-}
+};
 
 export default Footer;
