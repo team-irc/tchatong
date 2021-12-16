bf5f784333a7aff460b7b8e9c30a5e40ec1f08f0
Hna <ghnruf@naver.com>
2021-12-16 14:32:09 +0900

    Add Chatfire_DayTop Query Controller - 24시간 중 가장 높았던 분당 채팅수 출력 기능 추가

--- app/backend/src/chatfire/chatfire.resolver.ts
+++ app/backend/src/chatfire/chatfire.resolver.ts
@@ -11,8 +11,16 @@ export class ChatfireResolver {
     return this.chatfireService.findAll();
   }
 
+  /*
+   @brief 스트리머별 분당 채팅 수
+  */
   @Query((returns) => [Chatfire])
   async Chatfire_streamerNick(@Args('nick') nick: string): Promise<Chatfire[]> {
     return this.chatfireService.findOneByNick(nick);
+  }
+
+  @Query((returns) => Chatfire)
+  async Chatfire_dayTop(@Args('streamer_id') streamer_id: string): Promise<Chatfire> {
+    return this.chatfireService.findDayTop(streamer_id);
   }
 }
--- app/backend/src/chatfire/chatfire.service.ts
+++ app/backend/src/chatfire/chatfire.service.ts
@@ -21,4 +21,35 @@ export class ChatfireService {
     const streamer = await this.streamerRepository.findOne({ nick });
     return this.chatFireRepository.find({ streamer_id: streamer.streamer_id });
   }
+
+  private async getChatfiresAfterDate(streamer_id: string, date: Date): Promise<Chatfire[]> {
+    const sql = `SELECT * FROM chatfire WHERE (streamer_id = '${streamer_id}' AND date >= '${date}');`;
+    return (await this.chatFireRepository.query(sql));
+  }
+
+  private async initHighestChatfire(): Promise<Chatfire> {
+    const highest_chatfire = new Chatfire();
+    highest_chatfire.streamer_id = '';
+    highest_chatfire.id = 0;
+    highest_chatfire.date = new Date();
+    highest_chatfire.count = 0;
+    return (highest_chatfire);
+  }
+
+  private async getHighestCountFromChatfires(chatfires: Chatfire[]): Promise<Chatfire> {
+    let highest_chatfire = await this.initHighestChatfire();
+    for (let chatfire of chatfires) {
+      if (chatfire.count > highest_chatfire.count) {
+        highest_chatfire = chatfire;
+        highest_chatfire.count = chatfire.count;
+      }
+    }
+    return (highest_chatfire);
+  }
+
+  async findDayTop(streamer_id: string): Promise<Chatfire> {
+    const a_day_ago = new Date(new Date().getTime() - (24 * 60 * 60 * 1000));
+    const chatfires = await this.getChatfiresAfterDate(streamer_id, a_day_ago);
+    return (this.getHighestCountFromChatfires(chatfires));
+  }
 }
