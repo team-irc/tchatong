72fc44c1deb7ba7984177d0de46b8875e52d346a
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-12-23 12:28:15 +0900

    Add create issue api

--- app/backend/src/app.controller.ts
+++ app/backend/src/app.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { NewIssue } from './new-issue.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post('/issue')
+  async newIssue(@Body() newIssue: NewIssue) {
+    return await this.appService.createNewIssue(newIssue);
+  }
 }
