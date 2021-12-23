5ece1c051fe96e1b7169d926215d3c52ecb12612
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-12-23 15:01:50 +0900

    Fix cors

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -5,6 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+  	origin: ["https://www.tchatong.info", "http://127.0.0.1"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
