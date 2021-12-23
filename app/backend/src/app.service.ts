509fc79bb2029e6a559378022730ceac2d0bb6ac
AMATEUR_TOSS <amateur.toss@gmail.com>
2021-12-23 15:09:16 +0900

    Update contactus to use create issue api

--- app/backend/src/app.service.ts
+++ app/backend/src/app.service.ts
@@ -6,16 +6,16 @@ import { NewIssue } from './new-issue.dto';
 export class AppService {
   async createNewIssue(newIssue: NewIssue) {
     const oc = new Octokit({ auth: process.env.OCTOKIT_AUTH });
-    return await oc.rest.issues.create({
-      owner: 'team-irc',
-      repo: 'twitch-chat-analyzer-config',
-      title: `[${newIssue.issueType}] ${newIssue.body.slice(0, 10)}...`,
-      body: `
-      문의 종류: ${newIssue.issueType}
-      작성자 성함: ${newIssue.name}
-      작성자 이메일: ${newIssue.email}
-      본문: ${newIssue.body}
-      `,
-    });
+    try {
+      return await oc.rest.issues.create({
+        owner: 'team-irc',
+        repo: 'twitch-chat-analyzer-config',
+        title: `[${newIssue.issueType}] ${newIssue.body.slice(0, 10)}...`,
+        body: `문의 종류: ${newIssue.issueType}\n작성자 성함: ${newIssue.name}\n작성자 이메일: ${newIssue.email}\n본문: ${newIssue.body}`,
+        labels: ['question'],
+      });
+    } catch (e) {
+      return e;
+    }
   }
 }
