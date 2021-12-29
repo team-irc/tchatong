c04caa44e911e7e4a8fc9ca56ff49e589908f4e5
Hna <ghnruf@naver.com>
2021-12-29 12:13:24 +0900

    Add topward table

--- app/db/srcs/create_table.sql
+++ app/db/srcs/create_table.sql
@@ -30,3 +30,20 @@ CREATE TABLE IF NOT EXISTS legend(
   FOREIGN KEY (streamer_id) REFERENCES streamer(streamer_id) ON UPDATE CASCADE,
   FOREIGN KEY (chatfire_id) REFERENCES chatfire(id) ON UPDATE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS topword(
+  `id`                int          NOT NULL auto_increment PRIMARY KEY,
+  `streamer_id`       VARCHAR(32),
+  `date`              TIMESTAMP    NOT NULL,
+  `top1`              VARCHAR(16),
+  `top2`              VARCHAR(16),
+  `top3`              VARCHAR(16),
+  `top4`              VARCHAR(16),
+  `top5`              VARCHAR(16),
+  `top6`              VARCHAR(16),
+  `top7`              VARCHAR(16),
+  `top8`              VARCHAR(16),
+  `top9`              VARCHAR(16),
+  `top10`              VARCHAR(16),
+  FOREIGN KEY (streamer_id) REFERENCES streamer(streamer_id) ON UPDATE CASCADE
+);
