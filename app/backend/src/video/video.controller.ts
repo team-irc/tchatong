21c7238880d9f01f7918e24434812a14ca01e66d
Hna <ghnruf@naver.com>
2022-01-07 18:05:17 +0900

    Fix api time, send error code

--- app/backend/src/video/video.controller.ts
+++ app/backend/src/video/video.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { GetVideoDto } from './video.dto';
 import { VideoService } from './video.service';
 
@@ -9,7 +10,11 @@ export class VideoController {
 	) {}
 
 	@Get()
-	async getTwitchVideoUrl(@Query() getVideoDto: GetVideoDto) {
-		return this.videoService.getVideoUrl(getVideoDto.streamer_login, getVideoDto.time);
+	async getTwitchVideoUrl(@Query() getVideoDto: GetVideoDto, @Res() res: Response) {
+		const ret = await this.videoService.getVideoUrl(getVideoDto.streamer_login, getVideoDto.time);
+		if (ret.length != 0)
+			res.send(ret);
+		else
+			res.status(404).send("Video Not Found");
 	}
 }
