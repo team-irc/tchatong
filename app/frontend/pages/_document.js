a4c4bad8408dd4f83be55bfc8584a25aed77ec58
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-06-15 17:29:26 +0900

    feat: add tawk to

--- app/frontend/pages/_document.js
+++ app/frontend/pages/_document.js
@@ -23,17 +23,33 @@ class MyDocument extends Document {
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-RHM561R4NS"
           ></script>
-
+          {/* Google Analytics */}
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-RHM561R4NS', {
-              page_path: window.location.pathname,
-            });
-          `,
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-RHM561R4NS', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+          {/* Tawk To */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                  s1.async=true;
+                  s1.src='https://embed.tawk.to/62a9942eb0d10b6f3e776f47/1g5j6llaa';
+                  s1.charset='UTF-8';
+                  s1.setAttribute('crossorigin','*');
+                  s0.parentNode.insertBefore(s1,s0);
+                })();
+              `,
             }}
           />
         </Head>
