befa74b556c540410580aa71238b2947b708eb4a
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-06-21 10:12:23 +0900

    feat: implement Save method for bigQuery

--- app/backend/models/bigquery_m.go
+++ app/backend/models/bigquery_m.go
@@ -1,10 +1,24 @@
 package models
 
-import "time"
+import (
+	"cloud.google.com/go/bigquery"
+	"time"
+)
 
 type ChatLog struct {
 	StreamerId    string
 	StreamerLogin string
 	Date          time.Time
 	Content       string
+}
+
+// Save implements the ValueSaver interface.
+// This example disables best-effort de-duplication, which allows for higher throughput.
+func (i *ChatLog) Save() (map[string]bigquery.Value, string, error) {
+	return map[string]bigquery.Value{
+		"StreamerId":    i.StreamerId,
+		"StreamerLogin": i.StreamerLogin,
+		"Date":          i.Date,
+		"Content":       i.Content,
+	}, bigquery.NoDedupeID, nil
 }
