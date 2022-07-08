9c3a563b8b523967cfe26804904caea92a88efa7
AMATEUR_TOSS <amateur.toss@gmail.com>
2022-07-08 09:22:16 +0900

    fix: frontend build error

--- app/frontend/pages/_document.js
+++ app/frontend/pages/_document.js
@@ -37,7 +37,7 @@ class MyDocument extends Document {
             }}
           />
           {/* Google AdSense */}
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2606689224387389" crossorigin="anonymous" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2606689224387389" crossOrigin="anonymous" />
           {/* Tawk To */}
           <script
             dangerouslySetInnerHTML={{
